42778d8eb89854c0355be9862a27304f8a10aed0
esatsrc <34396890+esatsrc666@users.noreply.github.com>
2021-03-05 18:18:46 -0500

    Update MainActivity.java
    Just changed num

--- app/src/main/java/com/example/dunyasesi/MainActivity.java
+++ app/src/main/java/com/example/dunyasesi/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     int number = 5;
+    int newNum = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
