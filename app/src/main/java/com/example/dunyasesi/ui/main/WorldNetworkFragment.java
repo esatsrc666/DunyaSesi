2adfbbbef6996a042866c3a567ea6164778623c0
Mohamed Mnete <mnetemohamed@gmail.com>
2021-06-06 08:41:27 -0500

    added login/forgot password/register

--- app/src/main/java/com/example/dunyasesi/ui/main/WorldNetworkFragment.java
+++ app/src/main/java/com/example/dunyasesi/ui/main/WorldNetworkFragment.java
@@ -4,23 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.dunyasesi.R;
 
-/**
- * A placeholder fragment containing a simple view.
- */
-public class MyNetworkFragment extends Fragment {
+public class WorldNetworkFragment extends Fragment {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
 
-
-    public static MyNetworkFragment newInstance(int index) {
-        MyNetworkFragment fragment = new MyNetworkFragment();
+    public static WorldNetworkFragment newInstance(int index) {
+        WorldNetworkFragment fragment = new WorldNetworkFragment();
         Bundle bundle = new Bundle();
         bundle.putInt(ARG_SECTION_NUMBER, index);
         fragment.setArguments(bundle);
@@ -36,7 +31,7 @@ public class MyNetworkFragment extends Fragment {
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_my_network, container, false);
+        View root = inflater.inflate(R.layout.fragment_world_network, container, false);
         return root;
     }
 }
